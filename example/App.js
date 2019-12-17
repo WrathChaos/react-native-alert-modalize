3f5b1abdb105f02464b921c261290292e5441c34
FreakyCoder <kurayogun@gmail.com>
2019-12-17 13:19:51 +0300

    Library is completed.

--- example/App.js
+++ example/App.js
@@ -1,17 +1,41 @@
 import React from "react";
-import { StatusBar, SafeAreaView, TouchableOpacity } from "react-native";
+import { StatusBar, SafeAreaView, Text } from "react-native";
 import { AlertModalize } from "./lib/src/AlertModalize";
+import Ripple from "react-native-material-ripple";
 
 const App = () => {
   return (
     <>
       <StatusBar barStyle="dark-content" />
-      <SafeAreaView style={{ flex: 1 }}>
+      <SafeAreaView
+        style={{ flex: 1, alignItems: "center", justifyContent: "center" }}
+      >
         <AlertModalize ref={ref => (alertModal = ref)} />
-        <TouchableOpacity
-          style={{ height: 60, width: 300, backgroundColor: "#f1af01" }}
+        <Ripple
+          rippleColor="#fdfdfd"
+          rippleDuration={1250}
+          rippleContainerBorderRadius={16}
+          style={{
+            height: 60,
+            width: 300,
+            bottom: "20%",
+            borderRadius: 16,
+            alignItems: "center",
+            justifyContent: "center",
+            backgroundColor: "#f1af01"
+          }}
           onPress={() => alertModal.openModal()}
-        />
+        >
+          <Text
+            style={{
+              color: "#fdfdfd",
+              fontSize: 20,
+              fontFamily: "BurbankBigCondensed-Black"
+            }}
+          >
+            Open Modal
+          </Text>
+        </Ripple>
       </SafeAreaView>
     </>
   );
